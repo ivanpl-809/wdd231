1926197c991bcb5884d894f8a5db304e65b9692a
ivan-pl809 <pon20007@byui.edu>
2025-02-05 21:58:42 -0600

    commit

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,3 +1,16 @@
+// Dates
+const currentYear = new Date().getFullYear();
+document.getElementById("currentYear").textContent = currentYear;
+
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = `Last modification: ${lastModified}`;
+
+// Hamburger Toggle
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    document.getElementById('navbar').classList.toggle('show');
+});
+
+
 const visitMessage = document.getElementById('visit-message');
 const lastVisit = localStorage.getItem('lastVisit');
 const currentVisit = Date.now();
